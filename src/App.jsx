6995eeb26ce5247686e9e6c8048e45466b708394
Naveen <naveen.2101155@srec.ac.in>
2023-11-05 23:35:08 +0530

    trying edit bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import './App.css'
-import Footer from './components/Footer';
-import Navbar from './components/Navbar'
-import Title from './components/Title';
 import {Home,About,Research} from './components/pages'
+import Navbar from './components/Navbar'
+import Title from './components/Title'
+import Footer from './components/Footer'
 
-import { Routes, Route, Outlet } from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
-      <Title />
+    <Navbar />
+    <Title />
       <Routes>
-          <Route path="/rgb-react" element={<Outlet/>}>
-            <Route path="/" element={<Home/>}/>
-            <Route path="about" element={<About />} />
-            <Route path="research" element={<Research />} />
-          </Route>
+          <Route index element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="research" element={<Research />} />
       </Routes>
-      <Footer />
+    <Footer />
     </div>
   );};
 
